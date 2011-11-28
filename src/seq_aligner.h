d7761b750477c8ac931beb737299907f707134d3
brianchenming <brianchenming@gmail.com>
2011-11-27 23:36:07 -0500

    finish unit test of seq_aligner

--- src/seq_aligner.h
+++ src/seq_aligner.h
@@ -24,6 +24,7 @@
 #include	"common.h"
 
 // ratio is the maximum bias between segment and reference
+//#define DEBUG_ALIGNER
 #define R 0.3
 #define MAXN 2000
 #define MAXM (int)(MAXN*R)
@@ -37,7 +38,7 @@ enum OP {
 typedef struct {
     int score;        
     int parent;      
-} cell;
+} state;
 
 typedef struct {
     short op;
@@ -47,71 +48,86 @@ typedef struct {
 class seq_aligner {
 public:
     seq_aligner() {};
-    cell mat[MAXN][MAXM];       // DP matrix
-    edit edits[MAXN + MAXM];    // edits of transform pseg to pref[beg:end]
-    int l;                      // radius of diagonal stripe
+    int seg_len;                // max possible length of match in segment
+    int ref_len;                // max possible length of match in ref
+    int max_dst;                // max distance allowed
+    int seg_ml;                 // length of match in seg
+    int ref_ml;                 // length of match in ref
+    state mat[MAXN][MAXM];      // DP matrix
     int nedit;                  // number of edits
+    edit edits[MAXN + MAXM];    // edits of transform pseg to pref[beg:end]
     int align(seq_accessor *pseg, seq_accessor *pref) {
-        int n = pseg->length();
-        l = 1 + (int)(n * R);               // largest distance allowed
-        int m = MIN(pref->length(), n + l);
+        // work out parameters
+        if (pref->length() >= pseg->length()) { 
+            seg_len = pseg->length();
+            max_dst = 1 + (int)(seg_len * R);
+            ref_len = MIN(pref->length(), seg_len + max_dst);
+        } else {
+            ref_len = pref->length();
+            max_dst = 1 + (int)(ref_len * R);
+            seg_len = MIN(pseg->length(), ref_len + max_dst);
+        }
 
-        if (n >= MAXN || m >= MAXM) {
-            LOG("segment too long: %d\n", n);
+        if (seg_len >= MAXN || max_dst >= MAXM) {
+            LOG("segment too long: %d\n", seg_len);
             return -1;
         }
 
-        init_cell(n, m);
+        init_cell();
 
-        if (!search(pseg, pref, n, m, l)) return -1;
+        if (!search(pseg, pref)) return -1;
 
-        int ti, tj;
-        goal_cell(&ti, &tj, n, m);
-        if (ti < n*(1-R)) return -1;
+        goal_cell();
+        if (ref_ml < ref_len*(1-R)) return -1;
         nedit = 0;
-        find_path(ti, tj, pref);
+        find_path(seg_ml, ref_ml, pref);
 
-//        for (int i = 0; i < nedit; ++i) {
-//            printf("%d: %c\n", edits[i].op, edits[i].val);
-//        }
+#ifdef DEBUG_ALIGNER
+        print_matrix(pseg, pref);
+        printf("(%d, %d)\n", seg_ml, ref_ml);
+        printf("seg_len: %d, ref_len: %d\n", seg_len, ref_len);
+#endif
 
-        return tj;
+        return ref_ml;
     };
     int get_score(int i, int j) {
-        return mat[i][j-i+l].score;
+        return mat[i][j-i+max_dst].score;
     };
     void set_score(int i, int j, int v) {
-        mat[i][j-i+l].score = v;
+        mat[i][j-i+max_dst].score = v;
     };
     int get_parent(int i, int j) {
-        return mat[i][j-i+l].parent;
+        return mat[i][j-i+max_dst].parent;
     }
     void set_parent(int i, int j, int p) {
-        mat[i][j-i+l].parent = p;
+        mat[i][j-i+max_dst].parent = p;
+    }
+    int final_score() {
+        return get_score(seg_ml, ref_ml);
     }
 private:
     int match(char c, char d) { return c == d ? 5 : -4; };
     int indel(char c) { return -4; };
     // translate index into rectangle matrix to index into diagonal stripe
-    void init_cell(int n, int m) {
+    void init_cell() {
+        for (int i=0; i<=seg_len; ++i) {
+            int beg = MAX(1, i - max_dst);
+            int end = MIN(ref_len, i + max_dst);
+            for (int j=beg; j<=end; ++j) {
+                set_score(i, j, -4*MAX(i, j));
+                set_parent(i, j, i>j ? DELETE : (i==j ? MATCH : INSERT));
+            }
+        }
         set_score(0, 0, 0);
         set_parent(0, 0, 0);
-        for (int i=1; i<=n; ++i) {
-            set_score(i, 0, -4*i);
-            set_parent(i, 0, DELETE);
-        }
-        for (int j=1; j<=m; ++j) {
-            set_score(0, j, -4*j);
-            set_parent(0, j, INSERT);
-        }
     };
-    bool search(seq_accessor *pseg, seq_accessor *pref, int n, int m, int l) {
+    bool search(seq_accessor *pseg, seq_accessor *pref) {
         pseg->reset(0);     // start from the first
-        for (int i=1; i<=n; ++i) {
+        int best_score = 0;
+        for (int i=1; i<=seg_len; ++i) {
             char c = pseg->next();
-            int beg = MAX(1, i - l);
-            int end = MIN(m, i + l);
-            int best_score = 0;
+            int beg = MAX(1, i - max_dst);
+            int end = MIN(ref_len, i + max_dst);
             pref->reset(beg-1);     // start from the beg-th element
             for (int j=beg; j<=end; ++j) {
                 char d = pref->next();
@@ -139,17 +155,20 @@ private:
         }
         return true;
     };
-    void goal_cell(int *pti, int *ptj, int n, int m) {
-        *pti = *ptj = 1;
-        for (int i=1; i<=n; ++i) {
-            for (int j = 1; j<=m; ++j) {
-                if (mat[i][j].score > mat[*pti][*ptj].score) {
-                    *pti = i;
-                    *ptj = j;
+    void goal_cell() {
+        seg_ml = ref_ml = 0;
+        int best_score = get_score(0, 0);
+        for (int i=1; i<=seg_len; ++i) {
+            int beg = MAX(1, i - max_dst);
+            int end = MIN(ref_len, i + max_dst);
+            for (int j=beg; j<=end; ++j) {
+                if (get_score(i, j) > best_score) {
+                    seg_ml = i;
+                    ref_ml = j;
+                    best_score = get_score(i, j);
                 }
             }
         }
-        *ptj += *pti -l;
     };
     void find_path(int i, int j, seq_accessor *pref) {
         int p = get_parent(i, j);
@@ -171,6 +190,23 @@ private:
             ++nedit;
         }
     };
+    void print_matrix(seq_accessor *pseg, seq_accessor *pref) {
+        printf(" \t \t");
+        for (int j = 1; j <= ref_len; ++j) {
+            printf("%c\t", pref->at(j-1));
+        }
+        printf("\n");
+        for (int i = 0; i <= seg_len; ++i) {
+            printf("%c\t", i == 0 ? ' ' : pseg->at(i-1));
+            for (int j = 0; j <= ref_len; ++j) {
+                if (j < i-max_dst || j > i+max_dst) 
+                    printf("%d\t", -1);
+                else
+                    printf("%d\t", get_score(i, j));
+            }
+            printf("\n");
+        }
+    }
 };
 
 #endif
--- test/aligner_test.cpp
+++ test/aligner_test.cpp
@@ -16,24 +16,57 @@
 #include <seq_aligner.h>
 
 char dna_ref[] = "ACGTAACCGGTT";
-char dna_seg1[] = "CGTAAG";
+char dna_seg1[] = "CGTAAGC";
+char dna_seg2[] = "GTAACGGGTTAAAA";
 
-TEST(seq_aligner, basic1) {
+class aligner_test : public testing::Test {
+protected:
+    virtual void SetUp() {};
+    void edit_tester(seq_accessor *pref, seq_aligner *paligner) { 
+        int j = 0;      // index into dna_ref
+        for (int i = 0; i < paligner->nedit; ++i) {
+            char op = paligner->edits[i].op;
+            if (op == MATCH) { 
+                EXPECT_EQ(pref->at(j), paligner->edits[i].val);
+                ++j;
+            } else if (op == INSERT) {
+                EXPECT_EQ(pref->at(j), paligner->edits[i].val);
+                ++j; 
+            } 
+        }
+    };
+};
+
+TEST_F(aligner_test, overlay) {
     seq_aligner aligner;
-    seq_accessor ref((char*)dna_ref, true, 12);
-    seq_accessor seg((char*)dna_seg1, true, 6);
-    EXPECT_EQ(6, aligner.align(&seg, &ref));
-    EXPECT_EQ(21, aligner.get_score(5, 6));
-
-    int j = 0;      // index into dna_ref
-    for (int i = 0; i < aligner.nedit; ++i) {
-        char op = aligner.edits[i].op;
-        if (op == MATCH) { 
-            EXPECT_EQ(ref.at(j), aligner.edits[i].val);
-            ++j;
-        } else if (op == INSERT) {
-            EXPECT_EQ(ref.at(j), aligner.edits[i].val);
-            ++j; 
-        } 
-    }
+    seq_accessor ref((char*)dna_ref + 2, true, 10);
+    seq_accessor seg((char*)dna_seg2, true, 14);
+    EXPECT_EQ(10, aligner.align(&seg, &ref));
+    EXPECT_EQ(41, aligner.final_score());
+    edit_tester(&ref, &aligner);
 }
+
+TEST_F(aligner_test, forward) {
+    seq_aligner aligner;
+    seq_accessor ref1((char*)dna_ref, true, 12);
+    seq_accessor seg1((char*)dna_seg1, true, 6);
+    EXPECT_EQ(6, aligner.align(&seg1, &ref1));
+    EXPECT_EQ(21, aligner.final_score());
+    edit_tester(&ref1, &aligner);
+
+    seq_accessor ref2((char*)dna_ref, true, 12);
+    seq_accessor seg2((char*)dna_seg1, true, 7);
+    EXPECT_EQ(7, aligner.align(&seg2, &ref2));
+    EXPECT_EQ(22, aligner.final_score());
+    edit_tester(&ref2, &aligner);
+}
+
+TEST_F(aligner_test, backward) {
+    seq_aligner aligner;
+    seq_accessor ref((char*)dna_ref + 7, false, 7);
+    seq_accessor seg((char*)dna_seg1 + 6, false, 7);
+    EXPECT_EQ(7, aligner.align(&seg, &ref));
+    EXPECT_EQ(26, aligner.final_score());
+    edit_tester(&ref, &aligner);
+}
+
